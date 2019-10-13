01947bf2072913e8731dbb83d96fb262cf29efb1
daniilazarnov <azarnow.d@gmail.com>
2019-10-13 20:01:30 +0300

    added rest example

---
+++ example/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -d "@oleg.json" -X POST -H "Content-Type: application/json" http://localhost:8080/api/account/create
+
+curl -d "@german.json" -X POST -H "Content-Type: application/json" http://localhost:8080/api/account/create
+
+curl -d "@operation.json" -X POST -H "Content-Type: application/json" http://localhost:8080/api/operation/send
